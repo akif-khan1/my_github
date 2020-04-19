9b333a1b705d4d95b017a632c7bbb952785ddcc4
akif_khan1 <akif_khan1@icloud.com>
2020-04-20 00:10:08 +0300

    app1 done and fixed json with 2nd app started

--- Udemy/udemy_first_app.py
+++ Udemy/udemy_first_app.py
@@ -1,11 +1,34 @@
 import json
 from difflib import get_close_matches
-
+                                                                            # >>> SequenceMatcher(None, "rain", "rainn").ratio()
 data = json.load(open('data.json'))
 
 def translate(w):
-    return data[w]
+    w = w.lower()
+    if w in data:
+        return data[w]
+    elif w.title() in data:
+        return data[w.title()]
+    elif w.upper() in data:
+        return data[w.upper()]
+    elif len(get_close_matches(w, data.keys())) > 0 :
+        yn = input("Did you mean %s instead? Enter Y if yes or N if No: " % get_close_matches(w, data.keys())[0])
+        if yn == "Y":
+            return data[get_close_matches(w, data.keys())[0]]
+        elif yn == "N":
+            return "Word doesn't exist, please double check it!"
+        else:
+            return "Sorry! Unable to process the text."
+    else:
+        return "Word doesn't exist, please double check it!"
 
 word = input('enter a word to translate!\n     ""')
 
-print (translate(word))
+output = (translate(word))
+
+if type(output) == list:
+    print ("\n".join(output))
+    # for item in output:       # I used join method instead of iterate with for loop #
+    #     print(item)
+else:
+    print (output)
